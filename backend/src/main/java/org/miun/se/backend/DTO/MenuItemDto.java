fecb0a75b6d2534f823a38c90039dfefbf6cd6eb
Karamitsas <apostoliskaramitsas@hotmail.com>
2026-02-20 23:44:36 +0100

    Changed DTO class to implement Records

--- backend/src/main/java/org/miun/se/backend/DTO/MenuItemDto.java
+++ backend/src/main/java/org/miun/se/backend/DTO/MenuItemDto.java
@@ -2,39 +2,12 @@ package org.miun.se.backend.DTO;
 
 import java.time.LocalDate;
 
-public class MenuItemDto {
+public record MenuItemDto (
 
-    private String name;
-    private Integer menuItemId;
-    private String description;
-    private Double price;
-    private Boolean available;
-    private LocalDate mealDay;
-
-
-
-    public MenuItemDto(){}
-
-    public MenuItemDto(String name, Integer menuItemId, String description, Double price, Boolean available, LocalDate mealDay){
-        this.name = name;
-        this.menuItemId = menuItemId;
-        this.description = description;
-        this.price = price;
-        this.available = available;
-        this.mealDay = mealDay;
-    }
-
-    public String getName(){ return name;}
-    public Integer getMenuItemId(){ return menuItemId ;}
-    public String getDescription(){ return description ;}
-    public Double getPrice(){return price; }
-    public Boolean getAvailable() {return available;}
-    public LocalDate getMealDay() { return mealDay;}
-
-    public void setName(String name){ this.name = name;}
-    public void setMenuItemId(Integer menuItemId){ this.menuItemId = menuItemId; }
-    public void setDescription(String description) { this.description = description; }
-    public void setPrice(Double price){ this.price = price;}
-    public void setAvailable(Boolean available){ this.available = available; }
-    public void setMealDate(LocalDate mealDay) { this.mealDay = mealDay;}
-}
+    String name,
+    Integer menuItemId,
+    String description,
+    Double price,
+    Boolean available,
+    LocalDate mealDay
+) {}
--- backend/src/main/java/org/miun/se/backend/rest/LunchResource.java
+++ backend/src/main/java/org/miun/se/backend/rest/LunchResource.java
@@ -43,5 +43,4 @@ public class LunchResource {
 
         return results;
     }
-
 }
