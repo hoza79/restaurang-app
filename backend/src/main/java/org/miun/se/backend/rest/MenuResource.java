3828052c7285212db8d1e74ec7e8ff95bcdc9fdd
Karamitsas <apostoliskaramitsas@hotmail.com>
2026-02-24 00:21:26 +0100

    Added API (Delete, Post) functions for A la carte

--- backend/src/main/java/org/miun/se/backend/rest/MenuResource.java
+++ backend/src/main/java/org/miun/se/backend/rest/MenuResource.java
@@ -4,14 +4,24 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.Consumes;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.MediaType;
 import org.miun.se.backend.DTO.MenuDto;
 import org.miun.se.backend.DTO.MenuCarteItemDto;
+import org.miun.se.backend.DTO.CarteAddDto;
+import org.miun.se.backend.model.enums.MenuCartCategory;
 import org.miun.se.backend.model.MenuItem;
 import java.util.ArrayList;
 import java.util.List;
+import org.miun.se.backend.model.MenuCategory;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.PUT;
+
 
 @Path("/menu")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -63,5 +73,49 @@ public class MenuResource{
         return menu;
 
 
+    }
+
+    @POST
+    @Path("/{category}")
+    @Transactional
+    public Response addCarteMeal(@PathParam("category") MenuCartCategory category, CarteAddDto carteDto){
+        String databaseName = category.getDatabaseName();
+        MenuCategory menu;
+        try {
+            menu = em.createQuery(
+                    "SELECT menu FROM MenuCategory menu WHERE menu.categoryName = :name",
+                    MenuCategory.class).setParameter("name", databaseName)
+                    .getSingleResult();
+
+        } catch (Exception e) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        MenuItem item = new MenuItem(menu, carteDto.name(),
+                carteDto.description(), carteDto.price());
+        if(carteDto.available()){
+            item.setAvailable(carteDto.available());
+        }
+        em.persist(item);
+        return Response.ok().build();
+    }
+
+    @DELETE
+    @Path("/{itemId}")
+    @Transactional
+    public Response deletCarteMeal(@PathParam("itemId") Integer itemId){
+        MenuItem item;
+        try {
+            item = em.createQuery(
+                    "SELECT item FROM MenuItem item WHERE item.menuItemId = :id", MenuItem.class)
+                    .setParameter("id", itemId)
+                    .getSingleResult();
+
+        } catch (Exception e) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        em.remove(item);
+        return Response.ok().build();
     }
 }
